d9094de211f94a091c461c573f42a4b7882e09d5
Babayaga <babayagawoooh@gmail.com>
2025-09-26 15:15:57 +0530

    6

--- create-admin.ts
+++ create-admin.ts
@@ -1,10 +1,17 @@
-import { storage } from "./server/storage.js";
+import { storage } from "./server/storage.ts";
 import bcrypt from "bcrypt";
 import "dotenv/config";
 
 async function createAdmin() {
   const username = "admin"; // Change this to your desired username
   const password = "password"; // Change this to a strong password
+
+  // Check if admin already exists
+  const existingAdmin = await storage.getAdminByUsername(username);
+  if (existingAdmin) {
+    console.log(`Admin user "${username}" already exists.`);
+    process.exit(0);
+  }
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -14,4 +21,7 @@ async function createAdmin() {
   process.exit(0);
 }
 
-createAdmin();
+createAdmin().catch((err) => {
+  console.error("Failed to create admin user:", err);
+  process.exit(1);
+});
